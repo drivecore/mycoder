37b9a4756d457970e7de7193779158e58ebcc8a1
Ben Houston (via MyCoder) <neuralsoft@gmail.com>
2025-03-12 00:17:48 +0000

    refactor: remove interaction/index.ts to avoid re-export anti-pattern

--- packages/agent/src/tools/interaction/index.ts
+++
@@ -1,4 +0,0 @@
-export { agentMessageTool } from './agentMessage.js';
-export { agentStartTool, agentStates } from './agentStart.js';
-export { subAgentTool } from './subAgent.js';
-export { userPromptTool } from './userPrompt.js';
